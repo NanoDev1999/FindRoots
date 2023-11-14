dacadb0f61b44e22f9277f4877108ebc7c0aa897
jekdev1999 <jekdev1999@gmail.com>
2023-11-14 15:46:05 -0500

    Added JS solution, modified some math to make it a little more clear.

--- FindRoots/Program.cs
+++ FindRoots/Program.cs
@@ -13,7 +13,7 @@ public class CalculateQuadraticEquation
 {
     static double[] FindRoots(double a, double b, double c) 
     {
-        double discriminant = b * b - 4 * a * c;
+        double discriminant = (b * b) - (4 * a * c);
         double root1 = ((-b + Math.Sqrt(discriminant)) / (2 * a));
         double root2 = ((-b - Math.Sqrt(discriminant)) / (2 * a));
         return new double[] { root1, root2 };
---
+++ FindRoots/Program.js
@@ -0,0 +1,15 @@
+﻿// This is the JavaScript solution
+
+function findRoots(a, b, c) {
+  const discriminant = (b ** 2) - (4 * a * c);
+  const root1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+  const root2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+  return [root1, root2];
+}
+
+const a = 2;
+const b = 10;
+const c = 8;
+
+const roots = findRoots(a, b, c);
+console.log("Roots: [" + roots + "]");
